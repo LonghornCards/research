daab2e0c1a85a1ad932aef1c29827c71292298d3
LonghornCards <longhorncardsatx@gmail.com>
2024-06-01 11:38:33 -0500

    scoreboard update

--- src/Page1.js
+++ src/Page1.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './App.css'; // Ensure you have the styles in App.css
 
 const Page1 = () => {
     return (
-        <div>
+        <div className="page-container">
             <h1>Longhorn Cards Scoreboard</h1>
             <iframe
-                src="http://127.0.0.1:8050"
+                src="http://127.0.0.1:8050" // Ensure this URL matches the one printed by your Dash app
                 style={{ width: '100%', height: '600px', border: 'none' }}
                 title="Scoreboard"
             ></iframe>
