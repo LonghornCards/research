0ae2e8df95c1ce1bbfdf397b6614ea5eab89921b
LonghornCards <longhorncardsatx@gmail.com>
2024-06-01 11:24:39 -0500

    scoreboard.py

--- src/Page1.js
+++ src/Page1.js
@@ -6,9 +6,9 @@ const Page1 = () => {
             <h1>Longhorn Cards Scoreboard</h1>
             <iframe
                 src="http://127.0.0.1:8050"
-                style={{ width: '100%', height: '100vh', border: 'none' }}
-                title="Longhorn Cards Scoreboard"
-            />
+                style={{ width: '100%', height: '600px', border: 'none' }}
+                title="Scoreboard"
+            ></iframe>
         </div>
     );
 };
