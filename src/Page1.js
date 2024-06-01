062e7aa1dab126c7decaf9fc47dad170008a3f9f
LonghornCards <longhorncardsatx@gmail.com>
2024-06-01 13:31:29 -0500

    scoreboard update

--- src/Page1.js
+++ src/Page1.js
@@ -6,7 +6,7 @@ const Page1 = () => {
         <div className="page-container">
             <h1>Longhorn Cards Scoreboard</h1>
             <iframe
-                src="http://127.0.0.1:8050" // Ensure this URL matches the one printed by your Dash app
+                src="http://127.0.0.1:8050/" // Ensure this URL matches the one printed by your Dash app
                 style={{ width: '100%', height: '600px', border: 'none' }}
                 title="Scoreboard"
             ></iframe>
