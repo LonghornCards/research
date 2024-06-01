d6c41b95e02c83d1b859056c5644fb9a3bc2bf0e
LonghornCards <longhorncardsatx@gmail.com>
2024-06-01 03:05:01 -0500

    code update

--- src/App.js
+++ src/App.js
@@ -11,9 +11,15 @@ function App() {
             </header>
             <img src="https://github.com/LonghornCards/research/raw/main/LogoSimple.jpg" alt="Logo" className="logo" />
             <p>
-                This page is under construction. Please visit our sister site at {' '}
+                This page is under construction. Please visit our shop at: {' '}
                 <a href="https://www.longhorncardsatx.com" target="_blank" rel="noopener noreferrer">
                     Longhorn Cards & Collectibles
+                </a>.
+            </p>
+            <p>
+                Read our current Blog for research & analytics for the sports card market: {' '} 
+                <a href="https://longhorncardsatx.com/blogs/news" target="_blank" rel="noopener noreferrer">
+                    Blog
                 </a>.
             </p>
         </div>
