4bf2ae9e7f39fc75207a9155a9e3821ae1de5c90
LonghornCards <longhorncardsatx@gmail.com>
2024-06-01 10:36:51 -0500

    storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "s3websiteappstoragefdb68492": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
