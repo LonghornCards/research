fa3e2b3c3aa13799c84728d2f7b27f8d4aafae4f
LonghornCards <longhorncardsatx@gmail.com>
2024-07-20 23:37:11 -0500

    history update

--- src/PageHistory.js
+++ src/PageHistory.js
@@ -220,6 +220,66 @@ const PageHistory = () => {
                         <p>
                             In essence, sports cards are more than just collectibles; they are a celebration of sports heritage, a testament to human creativity and innovation, and a cherished part of popular culture that will continue to captivate and inspire for generations to come.
                         </p>
+                    </section>
+                    <section className="content-section">
+                        <h2>Retail vs. Hobby Box Products</h2>
+                        <p>
+                            Hobby boxes are higher-end boxes of sports cards that will often include more chase cards than retail boxes. Hobby boxes also tend to have higher price tags on them since they will give you a better chance to pull sports cards that could be more valuable than those found in retail boxes.  You'll also find special releases of hobby products, including 1st Off The Line products, Sapphire products, Jumbo products, Super Jumbo products, HTA products, Choice products, among others.
+                        </p>
+                        <p>
+                            A retail box is the type of sports card box you will find at most big-box stores such as Walmart and Target. Retail products can come in a variety of configurations, including Blaster Boxes, Mega Boxes, Value Boxes, Retail Boxes, Multi-Packs, Value Packs, Cello Packs, Rack Packs, Retail Packs, Factory Sets, Hanger Boxes, among others.  Unlike hobby boxes that are designed to come with more chase cards in them, the chase cards in retail boxes will be few and far between. It's possible to open multiple packs from a retail box without finding a single chase card.
+                        </p>
+                        <p>
+                            Hobby boxes are targeted for the hardcore collector, and generally focus on the premium aspects of a product like autographs and memorabilia cards. Odds of finding rare cards are typically higher.  Most hobby products guarantee one signature or relic per box whereas retail blaster boxes do not carry such minimum guarantees. Plus, often hobby boxes are the only product that contain certain card varieties and parallels.  Typically, high-end products are hobby-exclusive and will not be available in a retail format.
+                        </p>
+                    </section>
+                    <section className="content-section">
+                        <h2>Understanding Card Varieties</h2>
+                        <p>
+                            The introduction and popularity of parallel, variants, and refractor cards have had a significant influence on the sports card market. The rarity and uniqueness of these cards have added a new dimension to card valuations. The price of a card is no longer determined solely by the player it represents or its condition. The type of parallel or refractor, the scarcity, and even the aesthetic appeal can significantly impact a card's market value.
+                        </p>
+                        <p>
+                            Base cards are the building blocks of any sports card collection, and they make up the set's numbering scheme and sometimes reference how many are in the set.  Insert cards, sometimes called "chase cards" are tucked within sports card packs that were introduced in the 1990s. These cards are not typically part of a base set numbering system and often showcase unique themes, different designs, or other special features to make them distinct from the set with which they are packed.  They include autographed cards, relic cards, parallel cards, and special edition cards. 
+                        </p>
+                        <p>
+                            Parallel cards offer an intriguing twist to base cards. These cards will mirror the design and layout of a base card, but with distinguishing features that may be minor or could significantly change the look of the card. Common parallel variations include color schemes, borders, or foil accents.  Refractors are parallel cards with a reflective layer that can reflect light in a rainbow pattern at the proper angle that have been produced by various trading card companies for more than 25 years. These cards often appear in their own numbering scheme and are further divided by difference in color or another factor that allows the publisher to make more variants for a card set.
+                        </p>
+                        <p>
+                            Most rare among the refractors is the SuperFractor, which is usually a term reserved for a single-card population print. Like some refractors, these cards will almost always call out their uniqueness as "1/1" or "One of One" tags on the card. This feature is often paired with relics and game-worn components.  Companies that produce SuperFractors will sometimes release the one card directly to market through their website, commanding their own price for this one-of-a-kind card. Other companies will place them into the general population, releasing details about how rare the card is and the percentage chance of getting this treasure by opening a whole lot of boxes of cards.
+                        </p>
+                        <p>
+                            While base cards typically have a numbering scheme that shows which number among the set all copies of that card represent, special serial numbers for limited edition cards instead list the number for individual cards among a single print of that card. There can also be additional value derived from cards that have a lower serial number from a set.  When a card is part of a limited population, they will have a number that is not referencing the box's set, but the individual card's number from the printed population of the same card.
+                        </p>
+                        <p>
+                            Autographed cards are some of the most sought-after sports cards. They contain the signature of the featured athlete, typically signed directly on the card. Panini, Topps, and Upper Deck often include autographed cards in their sets, and these cards carry a premium due to their personal touch from athletes.  Relics in sports cards are small pieces of game-used jerseys or equipment that are embedded within the card. These relics are highly collectible and among the most sought-after by sports card enthusiasts and collectors since they provide a tangible connection to the player or team and offer a unique piece of memorabilia that can't be found elsewhere.
+                        </p>
+                        <p>
+                            Die-cut sports cards are different from regular sports cards in several ways. First, they are often cut into unique shapes that are different from the regular rectangular shape of traditional sports cards. They often have unusual design elements to make them more visually appealing and appropriate for their peculiar shape. Die-Cut cards also tend to be rarer due to their special status, and they are often also refractors and/or relic cards to just add further value to them.
+                        </p>
+                        <p>
+                            Finally, booklet cards are unique, fold-out cards that resemble a small book. They often feature a combination of elements, such as autographs, game-used memorabilia, and player photos or artwork. Booklet cards are produced for special events in limited quantities and usually numbered to indicate their scarcity.  The value of booklet cards is influenced by factors such as the featured players, the overall design, and the rarity of the card.
+                        </p>
+                    </section>
+                    <section className="content-section">
+                        <h2>Sports Card Grading Companies</h2>
+                        <p>
+                            During the pandemic, there was a massive increase of new collectors and grading baseball cards exploded.  Grading has become an essential aspect of the collectibles market, providing authenticity and value to cards.  There are many grading companies to choose from, but just a handful that have strong reputations in the industry:  Professional Sports Authenticator (PSA), Beckett Grading Services (BGS), Sportscard Guaranty Corporation (SGC), Certified Guaranty Corporation (CGC), and Hybrid Grading Approach (HGA).
+                        </p>
+                        <p>
+                            Founded in 1991, PSA has become one of the largest and most respected card grading services in the world.  They handle more than 75% of all cards graded in the US, and in the first half of 2023 alone PSA graded over 6.4 million cards out of an industry total of roughly 8 million. (See Figure 40, Source: Edge) It is known for its detailed grading scale ranging from 1 to 10, with 10 being a virtually perfect card.  PSA's encapsulation process preserves the card's condition while ensuring its authenticity. Their grading system is widely recognized and trusted, often leading to a higher resale value for cards graded by PSA. They also offer a comprehensive database of graded cards, providing valuable information for collectors.
+                        </p>
+                        <p>
+                            Beckett is a well-known name in the sports card industry, with its grading service, BGS, established in 1999. They are especially renowned for grading newer cards.  BGS uses a unique grading scale that includes half-point grades, offering a more nuanced assessment of a card's condition. Their graded cards are encapsulated in tamper-evident cases. Beckett is particularly known for its thorough assessment of a card's corners, edges, surface, and centering.
+                        </p>
+                        <p>
+                            SGC has been in the grading business since 1998 and has built a reputation for its focus on older, vintage cards.  SGC is known for its straightforward grading scale and fast turnaround times. They provide a tuxedo-like black matting within their encapsulation, which enhances the card's visual appeal. Their focus on vintage cards makes them a go-to for collectors of historic baseball memorabilia.
+                        </p>
+                        <p>
+                            While newer to the sports card grading scene compared to others, CGC has quickly established a strong reputation, leveraging its expertise from comic book grading.  CGC stands out for its state-of-the-art holder design, which offers superior protection and display qualities. They are also known for their transparency, providing detailed reports on the grading process for each card.
+                        </p>
+                        <p>
+                            HGA is a newer entrant in the card grading industry, bringing innovation and technology to the forefront.  HGA uses computer software for initial grading assessments, aiming to provide more consistent and objective grading. They also offer custom-colored slabs tailored to the card or team colors, which is unique in the industry.  HGA claims to have revolutionized the collectible card market with a novel software approach. This approach involves scanning, analyzing, and grading cards at high resolution without subjectivity, aiming to detect imperfections that human eyes may often overlook.
+                        </p>
                         <p>
                             -- Author: Longhorn Cards & Collectibles | July 2024 --
                         </p>
@@ -369,6 +429,22 @@ const PageHistory = () => {
                     <h3>Figure 36:  Technology Will Continue To Shape The Hobby</h3>
                     <a href="https://websiteapp-storage-fdb68492737c0-dev.s3.us-east-2.amazonaws.com/Trading+Card+History/History_Image45.jpeg" target="_blank" rel="noopener noreferrer">
                         <img src="https://websiteapp-storage-fdb68492737c0-dev.s3.us-east-2.amazonaws.com/Trading+Card+History/History_Image45.jpeg" alt="Trends of the Future 3" className="expandable-image" />
+                    </a>
+                    <h3>Figure 37:  Hobby vs. Retail Products</h3>
+                    <a href="https://websiteapp-storage-fdb68492737c0-dev.s3.us-east-2.amazonaws.com/Trading+Card+History/History_Image46.png" target="_blank" rel="noopener noreferrer">
+                        <img src="https://websiteapp-storage-fdb68492737c0-dev.s3.us-east-2.amazonaws.com/Trading+Card+History/History_Image46.png" alt="Trends of the Future 3" className="expandable-image" />
+                    </a>
+                    <h3>Figure 38:  Sports Trading Card Variations</h3>
+                    <a href="https://websiteapp-storage-fdb68492737c0-dev.s3.us-east-2.amazonaws.com/Trading+Card+History/History_Image47.png" target="_blank" rel="noopener noreferrer">
+                        <img src="https://websiteapp-storage-fdb68492737c0-dev.s3.us-east-2.amazonaws.com/Trading+Card+History/History_Image47.png" alt="Trends of the Future 3" className="expandable-image" />
+                    </a>
+                    <h3>Figure 39:  Sports Card Grading Company Comparison</h3>
+                    <a href="https://websiteapp-storage-fdb68492737c0-dev.s3.us-east-2.amazonaws.com/Trading+Card+History/History_Image48.png" target="_blank" rel="noopener noreferrer">
+                        <img src="https://websiteapp-storage-fdb68492737c0-dev.s3.us-east-2.amazonaws.com/Trading+Card+History/History_Image48.png" alt="Trends of the Future 3" className="expandable-image" />
+                    </a>
+                    <h3>Figure 40:  Sports Card Grading Survey</h3>
+                    <a href="https://websiteapp-storage-fdb68492737c0-dev.s3.us-east-2.amazonaws.com/Trading+Card+History/History_Image49.png" target="_blank" rel="noopener noreferrer">
+                        <img src="https://websiteapp-storage-fdb68492737c0-dev.s3.us-east-2.amazonaws.com/Trading+Card+History/History_Image49.png" alt="Trends of the Future 3" className="expandable-image" />
                     </a>
                 </div>
             </div>
